2182c61522f183347a40ff8d95615a219b3ee776
njfu <yingjief5312@gmail.com>
2014-03-16 19:31:03 -0500

    signup

--- web/static/toffee.js
+++ web/static/toffee.js
@@ -23,7 +23,7 @@ $('#login').on('click',function(){
     var email = $('#email').val();
     var password = $('#password').val();
     
-    socket.emit('signup', email, password);
+    socket.emit('login', email, password);
     
 });
     
@@ -36,6 +36,36 @@ $('#signup').on('click',function(){
     socket.emit('signup', email);
     
 });
+    
+//SIGNUP PW
+$('#pw-next').on('click',function(){
+    
+    var password = $('#signup-pw').val();
+    
+    socket.emit('password', password);
+    
+});
+    
+    
+//SIGNUP USERNAME
+$('#username-next').on('click',function(){
+    
+    var username = $('#signup-name').val();
+    
+    socket.emit('username', username);
+    
+});
+    
+    
+//FORGOT PW
+$('#iforgot').on('click',function(){
+    
+    var email = $('#email').val();
+    
+    socket.emit('iforgot', email);
+    
+});
+    
     
     
 socket.on('success', function() {
