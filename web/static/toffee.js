b628a992881a7c8b90ea6dad122be3ac2607419c
njfu <yingjief5312@gmail.com>
2014-03-16 20:13:00 -0500

    emit

--- web/static/toffee.js
+++ web/static/toffee.js
@@ -12,60 +12,48 @@ $("#question").keyup(function() {
     
     else if ($("#go").length == 0) {
         $("#box").append("<button id = 'go'>go</button>");
-    }
-    
+    } 
 });
     
     
 //LOG IN
 $('#login').on('click',function(){
-    
     var email = $('#email').val();
     var password = $('#password').val();
-    
     socket.emit('login', email, password);
-    
 });
     
     
 //SIGNUP
-$('#signup').on('click',function(){
+$('#signup-button').on('click',function(){
+    $('#signup-email').animate({right:'220px'},200);
+    var email = $('#signup-email').val();
+    socket.emit('signup-email', email);
+});
     
-    var email = $('#email').val();
-    
-    socket.emit('signup', email);
-    
+$('.background').on('click', function(){
+    $('#signup-email').animate({right:'10px'},200);
 });
     
 //SIGNUP PW
 $('#pw-next').on('click',function(){
-    
     var password = $('#signup-pw').val();
-    
-    socket.emit('password', password);
-    
+    socket.emit('signup-password', password);
 });
     
     
 //SIGNUP USERNAME
 $('#username-next').on('click',function(){
-    
     var username = $('#signup-name').val();
-    
-    socket.emit('username', username);
-    
+    socket.emit('signup-username', username);
 });
     
     
 //FORGOT PW
 $('#iforgot').on('click',function(){
-    
     var email = $('#email').val();
-    
     socket.emit('iforgot', email);
-    
 });
-    
     
     
 socket.on('success', function() {
