daffa2fb4a18fbc4520336ee71dfd5fb0755c815
weijinweng <weijin22@hotmail.com>
2014-03-22 05:15:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -336,7 +336,7 @@ io.sockets.on('connection', function (socket) {
 							else
 								{
 									console.log("serving page " + Title);
-									socket.emit('page-response',doc.title,doc.school, doc.description, doc.document);
+									socket.emit('page-response',doc.title,doc.school, doc.description, doc.document,doc.tags);
 								}
 						});
 				});
